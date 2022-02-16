07a96bda8075393509e07bd67ebc6e6e4898b666
Maverick9081 <faliyaabhishek@gmail.com>
2022-02-17 00:15:18 +0530

    Some changes

--- contracts/MarketPlace.sol
+++ contracts/MarketPlace.sol
@@ -3,16 +3,18 @@
 pragma solidity ^ 0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "./ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 contract MarketPlace {
     uint private nftId; 
     IERC20 public Token;
+    IERC721 public NFT;
     address payable admin;
 
-    constructor(address tokenAddress) {
+    constructor(address tokenAddress,address nftAddress) {
         Token = IERC20(tokenAddress);
         admin = payable(msg.sender);
+        NFT = IERC721(nftAddress);
     }
 
     struct MarketNft {
@@ -28,24 +30,34 @@ contract MarketPlace {
 
     function listNft(address nftContractAddress,uint tokenId, uint price) public {
         nftId++;
+        address user = msg.sender;
 
         market[nftId] = MarketNft(
             nftId,
             nftContractAddress,
             tokenId,
-            payable (msg.sender),
+            payable(user),
             price,
             false
-        );  
+        );
+        NFT.transferFrom(user,address(this),tokenId);
     }
 
-    function buyNft(address i,uint id) public {
-        uint price = market[id].price;
+    function buyNft(uint id) public {
+        address user = msg.sender;
+        
         uint tokenId = market[id].tokenId;
 
-        Token.approve(address(this),100);
-        IERC20(i).transfer(admin,50);
+        payement(id);
+        NFT.transferFrom(address(this),user,tokenId);
+    }
+
+    function payement(uint id) internal {
+        address user = msg.sender;
+        uint price = market[id].price;
+
+        Token.transferFrom(user,admin,price);
+
     }
 
 }
-
