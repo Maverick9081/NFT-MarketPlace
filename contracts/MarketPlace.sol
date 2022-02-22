24b7871af831c7472e5bae70aca334da3d7b67fd
Maverick9081 <faliyaabhishek@gmail.com>
2022-02-22 17:55:08 +0530

    Adding scripts and contract Changes#

--- contracts/MarketPlace.sol
+++ contracts/MarketPlace.sol
@@ -10,6 +10,7 @@ contract MarketPlace {
     IERC20 public Token;
     IERC721 public NFT;
     address payable admin;
+    uint private basefee = 250;
 
     constructor(address tokenAddress,address nftAddress) {
         Token = IERC20(tokenAddress);
@@ -17,11 +18,18 @@ contract MarketPlace {
         NFT = IERC721(nftAddress);
     }
 
+    struct RoyaltyInfo {
+        uint royaltyFraction;
+    }
+
+    mapping(uint => RoyaltyInfo) private tokenRoyaltyInfo;
+    mapping(uint => address[]) private RoyaltyOwners;
+
     struct MarketNft {
         uint id;
         address nftContractAddress;
         uint tokenId;
-        address owner;
+        address creator;
         address payable seller;
         uint price;
         bool sold;
@@ -29,7 +37,22 @@ contract MarketPlace {
 
     mapping(uint => MarketNft) private market;
 
-    function listNft(address nftContractAddress,uint tokenId, uint price) public {
+    //Balance of royalties received for an address
+    mapping(address => uint)private balances;
+
+    //settinng royalty of NFT to an address
+    //only creator can set NFT royalty
+    function setNftRoyalty (uint id,address royaltyReceiver,uint feeNumerator) public {
+        require(market[id].creator == msg.sender);
+        RoyaltyOwners[id].push(royaltyReceiver);
+        tokenRoyaltyInfo[id] = RoyaltyInfo(feeNumerator);
+    }
+
+    function getOwners(uint id) view public returns(address[] memory){ 
+        return RoyaltyOwners[id]; 
+    }
+
+     function listNft(address nftContractAddress,uint tokenId, uint price) public {
         nftId++;
         address user = msg.sender;
 
@@ -42,31 +65,43 @@ contract MarketPlace {
             price,
             false
         );
-        NFT.transferFrom(user,address(this),tokenId);
     }
 
     function buyNft(uint id) public {
+       
+        require(market[id].sold ==false);
         address user = msg.sender;
-        
+        address seller = market[id].seller;
         uint tokenId = market[id].tokenId;
-
-        payement(user,id);
-        NFT.transferFrom(address(this),user,tokenId);
-        market[id].owner = user;
-        market[id].sold = true;
-
-    }
-
-    function payement(address user, uint id) internal {
         uint price = market[id].price;
-        address payable seller = market[id].seller;
-        uint denominator =1000;
-        uint fees = price/(25/denominator);
-        Token.transferFrom(user,admin,fees);
-        uint amountReceivedBySeller = price -fees;
-        Token.transferFrom(user,seller,amountReceivedBySeller);
+
+        pay(price,user);
+        NFT.transferFrom(seller,user,tokenId);
+        payTheSeller(price,id,seller);
+        delegateBalancesToRoyalties(id,price);
+        delegateBalanceToAdmin(price);
+        market[id].seller = payable(user);
+        market[id].sold = true;
     }
 
+    //putting NFT on sale after buying from marketplace
+
+    function putNftOnSale(uint id,uint amount ) public {
+        require(market[id].seller == msg.sender);
+        market[id].sold = false;
+        market[id].price = amount;
+    }
+
+    //Reedem balance for the royalties received
+
+    function reddemBalance() public {
+        uint amount = balances[msg.sender];
+        require(amount > 100);
+        Token.transfer(msg.sender,amount);
+        balances[msg.sender] -= amount;
+    }
+
+    //displays NFTs on sale
     function onSaleNfts()public view returns(MarketNft [] memory){
         
         MarketNft[] memory items = new MarketNft[](nftId);
@@ -79,5 +114,54 @@ contract MarketPlace {
         }
         return items;
     }
+    
+    function getBalance(address account) public view returns(uint){
+        return balances[account];
+    }
 
+
+    function getTotalRoyaltyfees(uint salePrice,uint tokenId)internal view returns(uint){
+        uint feesNumerator =tokenRoyaltyInfo[tokenId].royaltyFraction;
+        uint owners = RoyaltyOwners[tokenId].length;
+        uint finalNumerator = feesNumerator * owners;
+        return salePrice*finalNumerator/_feeDenominator();
+    }
+    
+    function _feeDenominator() internal pure  virtual returns (uint96) {
+        return 10000;
+    }
+
+    
+    //allocates the Royalty fees to multiple royalty owners
+    function delegateBalancesToRoyalties(uint id,uint salePrice) internal{
+        uint owners = RoyaltyOwners[id].length;
+        uint fees =tokenRoyaltyInfo[id].royaltyFraction;
+
+        for(uint i=0 ; i<owners;i++){
+            address receiver = RoyaltyOwners[id][i];
+            uint amount = salePrice * fees/_feeDenominator();
+            balances[receiver] +=amount;
+        }
+    }
+
+    function adminFees(uint salePrice) internal view returns(uint){
+        uint amount = salePrice*basefee/_feeDenominator();
+        return amount;
+    }
+
+    function delegateBalanceToAdmin(uint salePrice) internal {
+        uint amount = adminFees(salePrice);
+        balances[admin] +=amount;
+    }
+
+    function payTheSeller(uint salePrice,uint id,address seller) internal {
+        uint amount = salePrice-adminFees(salePrice)-getTotalRoyaltyfees(salePrice,id);
+        Token.transfer(seller,amount);
+    }
+
+    //token payment from user to contract
+    function pay(uint price,address user) internal{
+        Token.transferFrom(user,address(this),price);
+    }
 }
+   
